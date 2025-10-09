f689504c567198f2774c41ba4bd2f3970c3c6aad
GabrieMartinsVirginio <gabriel.virginio@viannasempre.com.br>
2025-10-09 18:14:10 -0300

    feat: integrar SafeAreaProvider, PaperProvider e sistema de navegacao no App principal

--- steamverde/App.js
+++ steamverde/App.js
@@ -1,20 +1,20 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+import NavegacaoPrincipal from './src/view/components/navigator/NavegacaoPrincipal';
+import ProvedorAutenticacao from './src/view/components/authProvider/ProvedorAutenticacao';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>SteamVerde</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <PaperProvider>
+        <ProvedorAutenticacao>
+          <NavegacaoPrincipal />
+          <StatusBar style="auto" />
+        </ProvedorAutenticacao>
+      </PaperProvider>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
