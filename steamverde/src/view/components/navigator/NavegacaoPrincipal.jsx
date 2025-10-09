d2515d2110ae114b04a754ceb5a702fc7d8321e0
GabrieMartinsVirginio <gabriel.virginio@viannasempre.com.br>
2025-10-09 18:12:49 -0300

    feat: implementar navegacao principal com React Navigation Stack

---
+++ steamverde/src/view/components/navigator/NavegacaoPrincipal.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import TelaSplash from '../../TelaSplash';
+import Login from '../../Login';
+import Home from '../../Home';
+import TelaListaJogos from '../../jogos/TelaListaJogos';
+import TelaCadastroJogo from '../../jogos/TelaCadastroJogo';
+import TelaDetalheJogo from '../../jogos/TelaDetalheJogo';
+import TelaCarrinho from '../../carrinho/TelaCarrinho';
+import TelaPerfil from '../../perfil/TelaPerfil';
+
+const Stack = createNativeStackNavigator();
+
+const NavegacaoPrincipal = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator 
+        initialRouteName="Splash"
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Splash" component={TelaSplash} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="ListaJogos" component={TelaListaJogos} />
+        <Stack.Screen name="CadastroJogo" component={TelaCadastroJogo} />
+        <Stack.Screen name="DetalheJogo" component={TelaDetalheJogo} />
+        <Stack.Screen name="Carrinho" component={TelaCarrinho} />
+        <Stack.Screen name="Perfil" component={TelaPerfil} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default NavegacaoPrincipal;
